bd5506732ed188d295a40b516a937922fe84865e
aerosmis <ncrusemtoriz@gmail.com>
2026-04-24 14:39:16 +0200

    ajout fond gif + amélioration login

--- server.py
+++ server.py
@@ -66,4 +66,4 @@ def handle_disconnect():
     socketio.emit('users', list(set(users.values())))
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
